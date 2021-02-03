d605b45371b702d21a6ff3ae6aa7d455061d0938
opyum <florian.lorca@gmail.com>
2021-02-03 19:30:39 +0100

    Ajoutez des fichiers projet.

---
+++ Dibli/Areas/Login/Register.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dibli.Areas.Register
+{
+    public class Register : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View("/Views/Login/Register.cshtml");
+        }
+
+        // GET: Register/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Register/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Register/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Register/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Register/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Register/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Register/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
